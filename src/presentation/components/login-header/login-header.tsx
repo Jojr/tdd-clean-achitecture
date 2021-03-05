1a02b7e78bda1b651582e1b4f0b23d1b6c1bb935
João Belem Jr <junior.jb@gmail.com>
2021-03-04 21:55:30 -0300

    refactor: add multiple files export into index

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import Logo from '@/presentation/components/logo'
+import Logo from '@/presentation/components/logo/logo'
 import Styles from './login-header-styles.scss'
 
 const LoginHeader: React.FC = () => {
