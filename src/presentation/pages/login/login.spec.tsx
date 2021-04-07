0870293f078ba0a148f4ef419b9bd21e3c4e86ab
João Belem Jr <junior.jb@gmail.com>
2021-04-06 23:14:13 -0300

    test: ensure login show valid state if email is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -57,6 +57,16 @@ describe('Login component', () => {
     expect(passwordStatus.textContent).toBe('🔴')
   })
 
+  test('should show valid email state if validation succeeds', () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const emailStatus = sut.getByTestId('email-status')
+    expect(emailStatus.title).toBe('Tudo certo')
+    expect(emailStatus.textContent).toBe('🟢')
+  })
+
   test('should show valid password state if validation succeeds', () => {
     const { sut, validationStub } = makeSut()
     validationStub.errorMessage = null
